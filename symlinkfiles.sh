6546f783a4289c4a43a7cee0cda970e483d1433f
mduca <michael@mandu.ca>
2012-05-10 13:59:27 -0500

    add tmux dir for pane macros

--- symlinkfiles.sh
+++ symlinkfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile dircolors vimrc vim profile irbrc tmux.conf"   # list of files/folders to symlink in homedir
+files="bashrc bash_profile dircolors vimrc vim profile irbrc tmux.conf tmux"   # list of files/folders to symlink in homedir
 
 ##########
 
