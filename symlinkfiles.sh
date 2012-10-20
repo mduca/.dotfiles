b6cd20a8ddd6b03ed1b7d34153b41a9cdbb7ef74
mduca <michael@mandu.ca>
2012-10-20 17:29:08 -0500

    Script edit

--- symlinkfiles.sh
+++ symlinkfiles.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+# Create symlinks to desired dotfiles
+#
+# Sat 20 Oct 2012 04:10:02 PM CDT
 
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile dircolors vimrc vim profile irbrc tmux.conf tmux"   # list of files/folders to symlink in homedir
 
-##########
+# dotfiles directory
+dir=~/.dotfiles                    
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# list of files/folders to symlink in homedir
+files="vimrc vim irbrc tmux.conf" 
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# Create symlink
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/.dotfiles_old/
-    echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    echo -e "Symlink ->  \033[38;5;148m$file\033[39m"
 done
